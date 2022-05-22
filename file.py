41bfe5c08cd8818413703cc000c7da1a58a4c0af
JakubM <kobolte@gmail.com>
2022-05-22 13:48:53 +0200

    Zadanie 2.1 - greeter z argumentami

--- file.py
+++ file.py
@@ -1,5 +1,14 @@
+from functools import wraps
+
+
 def greeter(func):
-    pass
+    @wraps(func)
+    def inside_wrapper(*args):
+        get_name = func(*args)
+        return "Aloha " + get_name.lower().title()
+        
+    return inside_wrapper
+        
 
 
 def sums_of_str_elements_are_equal(func):
