5711477572cf2c7f146ce9684ffab8537a847fae
JakubM <kobolte@gmail.com>
2022-05-22 14:35:24 +0200

    Zadanie 2.2 - dekorator na sume stringów

--- file.py
+++ file.py
@@ -12,7 +12,50 @@ def greeter(func):
 
 
 def sums_of_str_elements_are_equal(func):
-    pass
+    @wraps(func)
+    def inside_wrapper(*args):
+        num_one, num_two = func(*args).split()
+        sum_one, sum_two = 0, 0
+
+        if(num_one[0] == '-'):
+            sum_one = sum([int(c) for c in num_one[1:]])
+        else:
+            sum_one = sum([int(c) for c in num_one])
+        
+        if(num_two[0] == '-'):
+            sum_two = sum([int(c) for c in num_two[1:]])
+        else:
+            sum_two = sum([int(c) for c in num_two])
+
+        if(sum_one == sum_two):
+            if(num_one[0] == '-' and num_two[0] == '-'):
+                    return "-" + str(sum_one) + " == -" + str(sum_two)
+            if(num_one[0] != '-' and num_two[0] != '-'):
+                    return str(sum_one) + " == " + str(sum_two)
+            if(num_one[0] != '-' and num_two[0] == '-'):
+                    return str(sum_one) + " != -" + str(sum_two)
+            if(num_one[0] == '-' and num_two[0] != '-'):
+                    return "-" + str(sum_one) + " != " + str(sum_two)
+        else:
+            if(num_one[0] == '-'):
+                if(num_two[0] == '-'):
+                    return "-" + str(sum_one) + " != -" + str(sum_two)
+                else:
+                    return "-" + str(sum_one) + " != " + str(sum_two)
+            else:
+                if(num_two[0] == '-'):
+                    return str(sum_one) + " != -" + str(sum_two)
+                else:
+                    return str(sum_one) + " != " + str(sum_two)
+                    
+    return inside_wrapper
+
+
+                    
+
+                
+            
+        
 
 
 def format_output(*required_keys):
